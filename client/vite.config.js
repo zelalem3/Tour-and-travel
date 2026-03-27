6f5bdfc2bb33cef518f5247fd6741de2c9deb376
zelalem3 <zgetnet24@gmail.com>
2026-03-27 19:05:55 +0300

    fix:removed  javascript-obfuscator for vercel build

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,21 +5,7 @@ import obfuscator from 'vite-plugin-javascript-obfuscator';
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      // 1. ONLY obfuscate your code (where your keys/logic are)
-      // This prevents the 'renderChunk' error by ignoring node_modules
-      include: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
-      exclude: [/node_modules/], 
-      
-      // 2. High-security settings that won't crash the Vercel build
-      compact: true,
-      controlFlowFlattening: false, // Critical to keep false on Vercel
-      deadCodeInjection: false,
-      stringArray: true,
-      stringArrayEncoding: ['base64'],
-      splitStrings: true,
-      unicodeEscapeSequence: true,
-    }),
+    
   ],
   build: {
     sourcemap: false,
