8d610a3b9d8bb8c28b37594cc9ab88064bb467b2
zelalem3 <zgetnet24@gmail.com>
2026-03-28 19:36:35 +0300

    fix: removed unecessary files from cms

--- travel-cms/sanity.cli.js
+++ travel-cms/sanity.cli.js
@@ -1,12 +1,16 @@
 import { defineCliConfig } from 'sanity/cli'
+require('dotenv').config();
+
+// Now you can access variables via process.env
+
 
 export default defineCliConfig({
   api: {
-    projectId: 'wzojjue2', // Your project ID
-    dataset: 'production'
+    projectId: process.env.PROJECT_ID, // Your project ID
+    dataset: process.env.DATASET
   },
   // Add this new section here:
   deployment: {
-    appId: 'ys0dm8a6u5f4623i8or6w9je',
+    appId: process.env.APP_ID,
   }
 })
