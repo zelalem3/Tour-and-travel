550673021c478f5015e3ea0e250be775f5ffa5fa
zelalem3 <zgetnet24@gmail.com>
2026-04-08 21:34:37 +0300

    feat: testimonial page added

--- travel-cms/schemaTypes/index.js
+++ travel-cms/schemaTypes/index.js
@@ -2,8 +2,9 @@ import { tourType } from './Tour'
 import { destinationType } from './destinationType'
 import contactInquiry from './ContactInquiry'
 import tailorMadeInquiry from './tailorMadeInquiry'
+import testimonial from './testimonial'
 import post from './post'
 import gallery from './gallery'
 import video from './video'
 
-export const schemaTypes = [tourType, destinationType, contactInquiry, tailorMadeInquiry, post, gallery, video]
+export const schemaTypes = [tourType, destinationType, contactInquiry, tailorMadeInquiry, post, gallery, video, testimonial]
